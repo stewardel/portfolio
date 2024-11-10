747f523c197df85726c315406ef00a122e2fd3be
stewardel <stewardmwanza28@gmail.com>
2024-11-10 23:23:31 +0100

    rows 5

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -40,7 +40,7 @@ const Contact = () => {
         <textarea
           className="bg-[#ccdf] p-2 "
           name="meassage"
-          rows="10"
+          rows="5"
           placeholder="Message"
         ></textarea><button className="text-white border-2 hover:bg-[#8892b0] hover:border-[#8892b0] mx-auto flex items-center">
           Let's Connect
