dc531c5377edcae375975d46705e1542fa9e5249
stewardel <stewardmwanza28@gmail.com>
2024-11-10 23:22:57 +0100

    connect

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -42,9 +42,10 @@ const Contact = () => {
           name="meassage"
           rows="10"
           placeholder="Message"
-        ></textarea><button className="text-black border-2 mx-auto flex items-center">
-         Let's Connect
-      </button> 
+        ></textarea><button className="text-white border-2 hover:bg-[#8892b0] hover:border-[#8892b0] mx-auto flex items-center">
+          Let's Connect
+        </button>
+        
       </form>
     </div>
   );
